543c4d37f5918ee125d521949f87ddfb6356f21b
timbdn01 <timbdn01@gmail.com>
2025-09-17 15:55:34 -0400

    Added player class and player object on screen

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS
+        
     print("Starting Asteroids!")
     print("Screen width:", constants.SCREEN_WIDTH)
     print("Screen height:", constants.SCREEN_HEIGHT)
